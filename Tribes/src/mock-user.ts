6034ba3f2a488c3566e91927147fee7cd1a243ca
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2019-05-14 12:51:57 +0200

    feat(Tribes):created header component, added images to the assets folder and created mock-user for test purposes

---
+++ Tribes/src/mock-user.ts
@@ -0,0 +1,10 @@
+import { User } from './app/user';
+
+export const MOCKUSER : User = {
+
+    username: 'Test User',
+    password: 'test123',
+    kingdomName: 'Test Kingdom',
+    userId: 1,
+    kingdomId: 1, 
+}
