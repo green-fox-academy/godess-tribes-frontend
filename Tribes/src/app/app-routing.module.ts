ac32ce650951090e2e4a9d00d171b4116b553902
Viola Pfening <pfeningviola@gmail.com>
2019-05-14 11:26:27 +0200

    fix style guide errors in app-routing.module

--- Tribes/src/app/app-routing.module.ts
+++ Tribes/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
@@ -9,4 +9,5 @@ const routes: Routes = [
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
+
 export class AppRoutingModule {}
