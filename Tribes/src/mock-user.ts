981593b7f4fe41c157f916f029268ff0fab3be35
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2019-05-14 15:18:04 +0200

    feat(Tribes)added style to the header component with Bootstrap

--- Tribes/src/mock-user.ts
+++ Tribes/src/mock-user.ts
@@ -4,7 +4,7 @@ export const MOCKUSER : User = {
 
     username: 'Test User',
     password: 'test123',
-    kingdomName: 'Test Kingdom',
+    kingdomName: 'Test Kingdom Name',
     userId: 1,
     kingdomId: 1, 
 }
