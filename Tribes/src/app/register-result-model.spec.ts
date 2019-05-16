39d30a0f62662062c60dca6308ca422381273bea
Gyulai Zsófia <gyulai.bozsofia@gmai.com>
2019-05-16 12:29:18 +0200

    created class

---
+++ Tribes/src/app/register-result-model.spec.ts
@@ -0,0 +1,7 @@
+import { RegisterResultModel } from './register-result-model';
+
+describe('RegisterResultModel', () => {
+  it('should create an instance', () => {
+    expect(new RegisterResultModel()).toBeTruthy();
+  });
+});
---
+++ Tribes/src/app/register-result-model.ts
@@ -0,0 +1,4 @@
+export class RegisterResultModel {
+    status: string;
+    message?: string;
+}
