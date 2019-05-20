bd89058dcb5ff48ff6b48f736488251cf5296f1b
Gyulai Zsófia <gyulai.bozsofia@gmai.com>
2019-05-20 14:02:59 +0200

    disabeling fields and button till waiting for data process

--- Tribes/src/app/register-form/register-form.component.ts
+++ Tribes/src/app/register-form/register-form.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {RegisterService} from '../register.service';
 import {UserService} from '../user.service';
+import { LoginService} from '../login.service';
 import {User} from '../user';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register-form',
@@ -9,7 +11,12 @@ import {User} from '../user';
   styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent implements OnInit {
-  constructor(private registerService: RegisterService) { }
+  constructor(
+    private registerService: RegisterService,
+    private loginService: LoginService,
+    private userService: UserService,
+    private router: Router)
+    { }
 
   user = new User();
   username: string;
@@ -18,16 +25,21 @@ export class RegisterFormComponent implements OnInit {
   submittedError: boolean;
   errorMessage: string;
   submitted = false;
+  processStarted = false;
 
   ngOnInit() {
   }
 
+  startProcess() {
+    this.processStarted = true;
+  }
 
   onSubmit({value, valid}): void {
     if (!valid) {
       this.submittedError = true;
       return;
     } else {
+      this.startProcess();
       this.submittedError = false;
 
       this.registerService.register(
@@ -37,10 +49,20 @@ export class RegisterFormComponent implements OnInit {
       )
       .subscribe(
         resp => {
+            this.processStarted = false;
             this.user.id = resp.id;
             this.user.username = resp.username;
             this.user.kingdomId = resp.kingdomId;
             this.submitted = true;
+            this.loginService.login(this.username, this.password)
+            .subscribe(
+              resp => {
+                if (resp.token) {
+                  this.userService.setToken(resp.token);
+                  this.router.navigateByUrl('/kingdom');
+                }
+              },
+            )
         },
         resp => {
           alert(resp.error.error);
--- Tribes/src/app/register.service.ts
+++ Tribes/src/app/register.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
-import { RegisterResultModel } from './register-result-model';
 import { User } from './user';
 
 @Injectable({
