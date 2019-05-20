f10fcaafbcb7b17b2227f0dba86d54d12af34050
Etterke <etterke@gmail.com>
2019-05-20 16:36:29 +0200

    feat(dashboard.component.html): removed *ngFor in html, listed icons manually

--- Tribes/src/app/mock-building.ts
+++ Tribes/src/app/mock-building.ts
@@ -4,10 +4,7 @@ import { Building } from './building';
 
 export const BUILDINGS: Building[] = [
     new Building (1, 'townhall', 1, 1, 2),
-    new Building (2, 'barrack', 1, 1, 2),
     new Building (3, 'mine', 1, 1, 2),
     new Building (4, 'farm', 1, 1, 2),
     new Building (5, 'farm', 1, 1, 2),
-    new Building (6, 'barrack', 1, 1, 2),
-    new Building (7, 'barrack', 1, 1, 2),
 ];
