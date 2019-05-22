51f1b195e79ce7da0f6bfb0b070215cd764d2540
Gyulai Zsófia <gyulai.bozsofia@gmai.com>
2019-05-22 13:00:32 +0200

    created error-handling service and moved handleError method into it

--- Tribes/src/app/register.service.ts
+++ Tribes/src/app/register.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { ErrorHandlingService } from './error-handling.service';
 
 import { User } from './user';
 
@@ -9,11 +10,10 @@ import { User } from './user';
   providedIn: 'root'
 })
 export class RegisterService {
- // rootUrl = 'http://virtserver.swaggerhub.com/szutsj/godess-tribes-backend/1.0.0';
- // rootUrl: string = 'https://app.swaggerhub.com/apis-docs/adamgyulavari/tribes/1.0.0#/'
   rootUrl = 'http://localhost:8080';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private errorHandlingService: ErrorHandlingService) { }
 
 
   register(username: string, password: string, kingdomName: string): Observable<User> {
@@ -23,26 +23,7 @@ export class RegisterService {
       kingdomName: kingdomName,
     })
     .pipe(
-      catchError(this.handleError)
+      catchError(this.errorHandlingService.handleError)
     );
   }
-
-  handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-      return throwError(`An error occurred: ${error.error.message}`);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error.message}`);
-        return throwError(
-          `${error.error.message} Please try an other one.`);
-    }
-    // return an observable with a user-facing error message
-    return throwError(
-      'Something bad happened; please try again later.');
-  };
 }
