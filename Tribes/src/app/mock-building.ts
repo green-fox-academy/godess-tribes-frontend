9b29c4c46d08ea743eb5927e392e8245d0d897ea
Etterke <etterke@gmail.com>
2019-05-22 12:02:27 +0200

    feat(dashboard.component.css): added style to remove white background

--- Tribes/src/app/mock-building.ts
+++ Tribes/src/app/mock-building.ts
@@ -16,5 +16,5 @@ export const BUILDINGS: Building[] = [
     new Building (3, 'farm', 1, 1, 2),
     new Building (3, 'farm', 1, 1, 2),
     new Building (4, 'farm', 1, 1, 2),
-    // new Building (5, 'barrack', 1, 1, 2),
+    new Building (5, 'barrack', 1, 1, 2),
 ];
