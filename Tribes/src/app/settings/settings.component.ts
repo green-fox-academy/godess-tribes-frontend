3062377930921ba0b4504381706b1dc20da65da7
Gyulai Zsófia <gyulai.bozsofia@gmai.com>
2019-05-27 12:22:58 +0200

    added getKingdomName method to component

--- Tribes/src/app/settings/settings.component.ts
+++ Tribes/src/app/settings/settings.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HeaderService } from '../header.service';
+
+
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  kingdomName: string;
 
-  constructor() { }
+  constructor(private headerService: HeaderService) { }
 
   ngOnInit() {
+  }
+
+  getKingdomName () {
+  this.headerService.getDataFromBackend().subscribe (response => this.kingdomName = response.kingdomName);
   }
 
 }
