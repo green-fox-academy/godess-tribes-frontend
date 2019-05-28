36c6c39cd0cc1a8adf960813d239c1f9059ad46c
Gyulai Zsófia <gyulai.bozsofia@gmai.com>
2019-05-28 14:52:14 +0200

    completed view and code in new component and checked in dashboard view

--- Tribes/src/app/add-new-badge/add-new-badge.component.ts
+++ Tribes/src/app/add-new-badge/add-new-badge.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { COST_NEW_BUILDING } from './../constants';
+
+import { ResourceService } from './../resource.service';
 
 @Component({
   selector: 'app-add-new-badge',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddNewBadgeComponent implements OnInit {
 
-  constructor() { }
+  goldAmount: number;
+  costOfNewBuilding = COST_NEW_BUILDING;
+
+  constructor(private resourceService: ResourceService) { }
 
   ngOnInit() {
+    this.getGoldAmount();
+  }
+
+  getGoldAmount() {
+    this.resourceService.getDataFromBackend()
+    .subscribe(response => this.goldAmount = response.resources
+      .find(resource => resource.type === 'GOLD').amount);
   }
 
 }
