de49cceaecea353122310977db6b9f54a3d78d07
Erika Kovács-Posta <posta.erika.mail@gmail.com>
2019-05-29 13:05:59 +0200

    feat(sidebar) made changes requested by Viola

--- Tribes/src/app/notifications/notifications.component.ts
+++ Tribes/src/app/notifications/notifications.component.ts
@@ -13,8 +13,6 @@ import { Building } from './../building';
 export class NotificationsComponent implements OnInit {
 
   listToDisplay: Notification[];
-  results: Building[];
-
   constructor() { }
 
   ngOnInit() {
