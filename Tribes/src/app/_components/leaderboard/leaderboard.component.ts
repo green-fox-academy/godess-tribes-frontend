a4085e768ac877860bc9cb50c6767ca647ba4b77
Gyulai Zsófia <gyulai.bozsofia@gmai.com>
2019-06-22 00:15:38 +0200

    retrieving value of path segment

--- Tribes/src/app/_components/leaderboard/leaderboard.component.ts
+++ Tribes/src/app/_components/leaderboard/leaderboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+
 import { LeaderboardService } from 'src/app/_services/leaderboard.service';
 import { LeaderboardItem } from 'src/app/_models/leaderboard-item';
 
@@ -9,16 +12,18 @@ import { LeaderboardItem } from 'src/app/_models/leaderboard-item';
 })
 export class LeaderboardComponent implements OnInit {
   kingdoms: LeaderboardItem[];
+  side : string;
 
-  constructor(private leaderboardService: LeaderboardService) { }
+  constructor(private leaderboardService: LeaderboardService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.getKingdoms();
+    // console.log(arguments, this.router.arguments);
   }
 
   getKingdoms () {
+    this.side = this.router.url;
     this.leaderboardService.getKingdomListWithBuildingsFromBackend().subscribe(response =>
       {this.kingdoms = response.leaderboard});
   }
-
 }
