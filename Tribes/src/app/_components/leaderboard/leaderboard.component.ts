3d7e9fcab6324f2edadbc59790f3a42374519a8b
Gyulai Zsófia <gyulai.bozsofia@gmai.com>
2019-06-23 11:57:25 +0200

    finished sorting observable array

--- Tribes/src/app/_components/leaderboard/leaderboard.component.ts
+++ Tribes/src/app/_components/leaderboard/leaderboard.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute } from '@angular/router';
 
 import { LeaderboardService } from 'src/app/_services/leaderboard.service';
 import { LeaderboardItem } from 'src/app/_models/leaderboard-item';
+import { KingdomResponse } from 'src/app/_models/kingdom-response';
+import { LeaderboardResponse } from 'src/app/_models/leaderboard-response';
 
 @Component({
   selector: 'app-leaderboard',
@@ -13,6 +15,7 @@ import { LeaderboardItem } from 'src/app/_models/leaderboard-item';
 export class LeaderboardComponent implements OnInit {
   kingdoms: LeaderboardItem[];
   side : string;
+  compareDate : number;
 
   constructor(private leaderboardService: LeaderboardService, private router: Router, private route: ActivatedRoute) { }
 
@@ -24,6 +27,12 @@ export class LeaderboardComponent implements OnInit {
   getKingdoms () {
     this.side = this.router.url;
     this.leaderboardService.getKingdomListWithBuildingsFromBackend().subscribe(response =>
-      {this.kingdoms = response.leaderboard});
+      {this.kingdoms = response.leaderboard.sort(this.compareData)});
+ /*    this.leaderboardService.getKingdomListWithSoldiersFromBackend().subscribe(response =>
+      {this.kingdoms = response.leaderboard}); */
   }
+
+  compareData = function(kingdom1: LeaderboardItem, kingdom2: LeaderboardItem) {
+    return kingdom1.buildings > kingdom2.buildings ? -1 : 1;
+  };
 }
